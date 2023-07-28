a354dd19faac80c09cde1bfafc6b8e0a4c61a772
Michel Jacques <michelm.jacques@gmail.com>
2023-07-27 19:45:41 -0700

    Can launch VS Code now

---
+++ Vagrantfile
@@ -0,0 +1,18 @@
+Vagrant.configure("2") do |config|
+        config.vm.define "supertimor" do |node|
+            node.vm.box = "ubuntu/xenial64"
+            node.vm.hostname = "supertimor"
+            node.vm.network :private_network, ip: "168.198.58.58"
+            node.vm.network "forwarded_port", guest: 22, host: 22, id: "ssh"
+            node.vm.boot_timeout = 300
+
+            node.vm.provider :virtualbox do |vb|
+                vb.customize ["modifyvm", :id, "--memory", 512]
+                vb.customize ["modifyvm", :id, "--cpus", 1]
+            end  # do node.vm.provider
+			node.vm.provision "file", source: "~/.ssh/keys_for_linux_vm_dev/code_on_linux_from_windows_addendum_authorize_keys.pub", destination: "/tmp/provision/temp_pub_key.pub"
+            node.vm.provision "file", source: "~/.ssh/keys_for_linux_vm_dev/vm_key_for_gitbuh.openssh_private", destination: "/tmp/provision/temp_private_key_for_github"
+			node.vm.provision "shell", path: "bin/vagrant_provision.sh"
+        end # config.vm.define
+    
+end # Vagrant.configure
---
+++ bin/vagrant_provision.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+#Create a development user different than vagrant
+useradd uruk
+
+echo "uruk ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/uruk
+chmod 0440 /etc/sudoers.d/uruk
+usermod --shell /bin/bash uruk
+
+mkdir -p /home/uruk/.ssh
+touch /home/uruk/.ssh/authorized_keys
+chmod 0700 /home/uruk/.ssh
+chmod 0644 /home/uruk/.ssh/authorized_keys
+
+chown -R uruk:uruk /home/uruk
+
+# Key Management
+# sudo su vagrant -c 'cat /tmp/provision/temp_pub_key.pub >> ~/.ssh/authorized_keys'
+# sudo su root -c 'cat /tmp/provision/temp_pub_key.pub >> ~/.ssh/authorized_keys'
+sudo su uruk -c 'cat /tmp/provision/temp_pub_key.pub >> ~/.ssh/authorized_keys'
+
+# SSH Configuration management
+if [ -z "$( cat /etc/ssh/sshd_config | grep AllowTcpForwarding)" ]
+then
+	echo "AllowTcpForwarding yes" >> /etc/ssh/sshd_config
+else
+	sed -i '/#AllowTcpForwarding yes/c\AllowTcpForwarding yes' /etc/ssh/sshd_config	
+fi
+
+systemctl restart sshd.service
+
+# Preparing a git environment that can connecto to my github repo
+mkdir -p /home/uruk/git
+chmod 0700 /home/uruk/git
+chown -R uruk:uruk /home/uruk/git
+
+## Specific key for Github.
+cp /tmp/provision/temp_private_key_for_github /home/uruk/.ssh/id_ed25519
+chmod 0600 /home/uruk/.ssh/id_ed25519
+chown -R uruk:uruk /home/uruk/.ssh/id_ed25519
+
+## Git author properties
+git config --global user.name "Michel JACQUES"
+git config --global user.email "michel.jacques@polymtl.ca"
+
+
+# Clean up
+rm -rf /tmp/provision
+
